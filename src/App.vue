dac57abc4fbadce24e9a5038ca8f4d7f10f9035f
jfcastel0 <jcas206@wgu.edu>
2022-04-24 21:25:48 -0400

    Added Headings

--- src/App.vue
+++ src/App.vue
@@ -12,16 +12,24 @@
     <Button  disabled>
       My Button
     </Button>
+    <Heading size="h1">Heading</Heading>
+    <Heading size="h2">Heading</Heading>
+    <Heading size="h3">Heading</Heading>
+    <Heading size="h4">Heading</Heading>
+    <Heading size="h5">Heading</Heading>
+    <Heading size="h6">Heading</Heading>
   </div>
 </template>
 
 <script>
   import Button from './components/Button.vue'
+  import Heading from './components/Heading.vue'
 
   export default {
     name: 'App',
     components: {
-      Button
+      Button,
+      Heading
     },
     data() {
       return {
---
+++ src/components/Heading.vue
@@ -0,0 +1,18 @@
+<template>
+    <component :is="size">
+        <slot></slot>
+    </component>
+</template>
+
+<script>
+    export default {
+        name: 'Heading',
+        
+        props: {
+            size: {
+                type: String,
+                default: 'h1'
+            }
+        }
+    }
+</script>
